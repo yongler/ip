cb1346b829afaaf689749f09f3d285610dfd5902
yongler <e0550517@u.nus.edu>
2022-01-18 22:51:07 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.net.SecureCacheResponse;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
@@ -14,14 +15,21 @@ public class Duke {
 
         System.out.println("Hello! I'm NewDuke");
         System.out.println("What can I do for you?");
+        ArrayList<String> arr = new ArrayList<>();
 
         while (true) {
             String str = sc.nextLine();
             if (str.compareTo("bye") == 0) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            }else if (str.compareTo("list") == 0) {
+                for (int i =0;i<arr.size();i++) {
+                    System.out.printf("%d. %s\n",i+1, arr.get(i));
+                }
+            } else {
+                arr.add(str);
+                System.out.println("Added: " + str);
             }
-            System.out.println(str);
         }
 
 
