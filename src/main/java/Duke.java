a38043c525faf74dc42825493292ba448dcd72d1
yongler <e0550517@u.nus.edu>
2022-01-18 22:37:10 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.net.SecureCacheResponse;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +9,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello! I'm NewDuke");
+        System.out.println("What can I do for you?");
+
+        while (true) {
+            String str = sc.nextLine();
+            if (str.compareTo("bye") == 0) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(str);
+        }
+
+
     }
 }
