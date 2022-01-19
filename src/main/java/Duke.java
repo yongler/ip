1dcf34e1f472f3c3b6c1e62ce2fd53c033fb286d
yongler <e0550517@u.nus.edu>
2022-01-19 21:31:55 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,12 @@
+import org.w3c.dom.events.EventException;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
+    protected static ArrayList<Task> tasks;
+
     public static class Task {
         protected String description;
         protected boolean isDone;
@@ -9,11 +14,9 @@ public class Duke {
         public Task(String description) {
             this.description = description;
             this.isDone = false;
+            printAdded();
         }
 
-        public void toggleDone() {
-            this.isDone = !this.isDone;
-        }
         public void markAsDone() {
             this.isDone = true;
         }
@@ -25,25 +28,68 @@ public class Duke {
             return (isDone ? "X" : " "); // mark done task with X
         }
 
+        public void printAdded() {
+            System.out.println("Got it. I've added this task: ");
+            System.out.println(this);
+            System.out.printf("Now you have %d tasks in the list.\n", tasks.size() + 1);
+        }
+
         @Override
         public String toString() {
             return String.format("[%s] %s", getStatusIcon(), description);
         }
     }
 
-//    public class Deadline extends Task {
+    public static class Deadline extends Task {
+        protected String by;
+
+        public Deadline(String description, String by) {
+            super(description);
+            this.by = by;
+        }
+
+        @Override
+        public void printAdded() {
+            System.out.println("Got it. I've added this task: ");
+            System.out.println(this);
+            System.out.printf("Now you have %d tasks in the list.\n", tasks.size() + 1);
+        }
+
+        @Override
+        public String toString() {
+            return "[D]" + super.toString() + "(by: " + this.by + ")";
+        }
+    }
+
+    public static class Todo extends Task {
+        protected String by;
+
+        public Todo(String description) {
+            super(description);
+        }
+
+        @Override
+        public String toString() {
+            return "[T]" + super.toString();
+        }
+    }
+
+    public static class Event extends Task {
+        protected String by;
+
+        public Event(String description, String by) {
+            super(description);
+            this.by = by;
+        }
+
+        @Override
+        public String toString() {
+            return "[E]" + super.toString() + "(by: " + by + ")";
+        }
+    }
+
+//    public static void addTask() {
 //
-//        protected String by;
-//
-//        public Deadline(String description, String by) {
-//            super(description);
-//            this.by = by;
-//        }
-//
-//        @Override
-//        public String toString() {
-//            return "[D]" + super.toString() + " (by: " + by + ")";
-//        }
 //    }
 
     public static void main(String[] args) {
@@ -59,9 +105,10 @@ public class Duke {
         System.out.println("Hello! I'm NewDuke");
         System.out.println("What can I do for you?");
 
-        ArrayList<Task> tasks = new ArrayList<>();
+        tasks = new ArrayList<>();
 
         while (true) {
+            System.out.println("-------------");
             String whole_str = sc.nextLine();
             String[] str = whole_str.split(" ");
 
@@ -83,10 +130,46 @@ public class Duke {
                 tasks.get(k - 1).markAsUndone();
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println(tasks.get(k - 1));
+            } else if (str[0].compareTo("deadline") == 0) {
+                String stuff = "";
+                String deadline = "";
 
+                for (int i =1;i< str.length;i++) {
+                    if (str[i].compareTo("/by") == 0) {
+                        for (int j =1;j<i;j++) {
+                            stuff += str[j] + " ";
+                        }
+                        for (int j =i+1;j<str.length;j++) {
+                            deadline += str[j] + " ";
+                        }
+                        break;
+                    }
+                }
+                tasks.add(new Deadline(stuff, deadline));
+            } else if (str[0].compareTo("todo") == 0) {
+                String stuff = "";
+                for (int i =1;i< str.length;i++) {
+                    stuff += str[i] + " ";
+                }
+                tasks.add(new Todo(stuff));
+            } else if (str[0].compareTo("event") == 0) {
+                String stuff = "";
+                String deadline = "";
+
+                for (int i =1;i< str.length;i++) {
+                    if (str[i].compareTo("/at") == 0) {
+                        for (int j =1;j<i;j++) {
+                            stuff += str[j] + " ";
+                        }
+                        for (int j =i+1;j<str.length;j++) {
+                            deadline += str[j] + " ";
+                        }
+                        break;
+                    }
+                }
+                tasks.add(new Event(stuff, deadline));
             } else {
                 tasks.add(new Task(whole_str));
-                System.out.println("Added: " + whole_str);
             }
         }
 
